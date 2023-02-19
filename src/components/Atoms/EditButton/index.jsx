aceccf06aea3e6c19ed7b837bb11c3399f083281
luck-tech <luck@imai.am>
2023-02-19 15:07:14 +0900

    paddingとアイコンの大きさ調整

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 
-const EditButton = (props) => {
+const EditButton = ({ onClick }) => {
   return (
-    <StyledButton onClick={props.onClick}>
+    <StyledButton onClick={onClick}>
       <ButtonCircle></ButtonCircle>
       <img src={pencil} />
     </StyledButton>
@@ -15,17 +15,24 @@ const EditButton = (props) => {
 const StyledButton = styled.button`
   background-color: transparent;
   border: none;
+  padding: 0;
+  width: 20px;
+  height: 20px;
+  & > img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 const ButtonCircle = styled.div`
-  width: 24px;
-  height: 24px;
+  width: 20px;
+  height: 20px;
   position: absolute;
   cursor: pointer;
   &:hover {
     background-color: ${COLOR.LIGHT_GRAY};
-    width: 24px;
-    height: 24px;
+    width: 20px;
+    height: 20px;
     opacity: 0.2;
     position: absolute;
     border-radius: 50%;
