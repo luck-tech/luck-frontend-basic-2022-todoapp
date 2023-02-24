408cc9fe7825290afcb8b96a2bff99832e408b90
luck-tech <luck@imai.am>
2023-02-24 20:54:47 +0900

    構造の整理とcssの調整

--- src/components/Atoms/Checkbox/index.jsx
+++ src/components/Atoms/Checkbox/index.jsx
@@ -6,7 +6,6 @@ import COLOR from "../../../variables/color";
 const Checkbox = ({ onClick }) => {
   return (
     <StyledCB onClick={onClick}>
-      <BoxFrame />
       <img src={check} />
     </StyledCB>
   );
@@ -18,24 +17,17 @@ const StyledCB = styled.button`
   width: 20px;
   height: 20px;
   background-color: transparent;
-  position: relative;
+  position: absolute;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
+  border-radius: 2px;
+  &:hover img {
+    display: block;
+  }
   & > img {
     width: 15px;
     height: 15px;
-    top: 2.5px;
-    left: 2.5px;
-    position: absolute;
-    &:hover {
-      display: none;
-    }
+    display: none;
   }
-`;
-
-const BoxFrame = styled.div`
-  border: 2px solid ${COLOR.LIGHT_GRAY};
-  border-radius: 2px;
-  width: 20px;
-  height: 20px;
 `;
 
 export default Checkbox;
